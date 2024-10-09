884082e25c5251f377e9044f8eb565c17edf175a
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-10-09 21:50:46 +0900

    생명주기 함수 render()

---
+++ src/R004_LifecycleEx.js
@@ -0,0 +1,13 @@
+import Reac, {Component} from "react";
+
+class R004_LifecycleEx extends Component{
+  render() {
+    console.log('3.render Call');
+    return (
+      <h2>[THIS IS RENDER FUNCTION]</h2>
+    )
+  }
+
+
+}
+export default R004_LifecycleEx;
