b067b152c94f36cb4aa1ff8cc9f530ac4cba96d9
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-10-14 21:52:07 +0900

    props boolean

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import './App.css';
-import PropsDataType from './R018_PropsDatatype';
+import PropsBoolean from './R019_PropsBoolean';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <PropsDataType
-        String = "react"
-        Number = {200}
-        Boolean = {1==1}
-        Array={[0, 1, 8]}
-        ObjectJson = {{react : "리액트", twohundred:"200"}}
-        Function={console.log("FunctionProps : function!")}
-        />
+      <PropsBoolean BooleanTrueFlase={false}/>
+      <PropsBoolean BooleanTrueFalse/>
     </div>
   );
 }
